8d9d598825b6171ed78ed39b5e169bca294e056c
Jonathan <dunlap@adobe.com>
2013-03-03 15:36:57 -0800

    first

---
+++ Source/EpicGameJam.hx
@@ -0,0 +1,49 @@
+package ;
+
+
+import nme.display.Sprite;
+import nme.display.StageAlign;
+import nme.display.StageScaleMode;
+import nme.Lib;
+
+
+/**
+ * @author Jonathan Dunlap
+ */
+class EpicGameJam extends Sprite {
+	
+	
+	public function new () {
+		
+		super ();
+		
+		initialize ();
+		
+		
+		
+	}
+	
+	
+	private function initialize ():Void {
+		
+		Lib.current.stage.align = StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		
+	}
+	
+	
+	
+	
+	// Entry point
+	
+	
+	
+	
+	public static function main () {
+		
+		Lib.current.addChild (new EpicGameJam ());
+		
+	}
+	
+	
+}
