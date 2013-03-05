4c0185694ebffb9630940c95d7c453fb0eafbcd8
Jonathan <dunlap@adobe.com>
2013-03-05 11:06:49 -0800

    more physics fluff

--- Source/mutator/Physics.hx
+++ Source/mutator/Physics.hx
@@ -1,6 +1,7 @@
 package mutator;
 import model.Movement;
 import model.MoveNode;
+import model.PhysicsBody;
 import model.Pos;
 import nme.errors.Error;
 
@@ -13,15 +14,18 @@ class Physics implements ISystem
 {
 	var posList:Array<Pos>;
 	var moveList:Array<Movement>;
+	var phyList:Array<PhysicsBody>;
 	
 	public function new() 
 	{	
 		moveList = new Array<Movement>();
 		posList = new Array<Pos>();
+		phyList = new Array<PhysicsBody>;
 	}
 	public function add(entity:Entity):Void {
 		posList.push(entity.fetch(Pos));
 		moveList.push(entity.fetch(Movement));
+		phyList.push(entity.fetch(PhysicsBody));
 	}
 	public function start():Void {
 		
@@ -34,6 +38,7 @@ class Physics implements ISystem
 		while (current < posList.length) {
 			var pos  = posList[current];
 			var movement = moveList[current];
+			var phy = phyList[current];
 			
 			pos.pt.y -= movement.vel.y;
 			current++;
